c974d43a6ab5f8196ec423b229d721cab25aacd9
DH.L <meozigoon@gmail.com>
2025-07-11 23:15:03 +0900

    v15.0
    docs updated

---
+++ docs/docs.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const links = document.querySelectorAll(".top-menu a");
+    const sections = document.querySelectorAll("main section");
+
+    if (links.length && sections.length) {
+        links[0].classList.add("active");
+        sections[0].classList.add("active");
+        sections[1].classList.add("active");
+    }
+
+    links.forEach((link) => {
+        link.addEventListener("click", function (event) {
+            event.preventDefault();
+            const targetId = this.getAttribute("href").substring(1);
+
+            links.forEach((l) => l.classList.remove("active"));
+            sections.forEach((s) => s.classList.remove("active"));
+            sections[0].classList.add("active");
+
+            this.classList.add("active");
+            const targetSection = document.getElementById(targetId);
+            if (targetSection) {
+                targetSection.classList.add("active");
+            }
+        });
+    });
+});
