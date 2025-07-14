da3f80931b208e272702ee9ac13eadf63ec54707
DH.L <meozigoon@gmail.com>
2025-07-14 22:43:57 +0900

    v15.8
    docs show edited

--- docs/docs.js
+++ docs/docs.js
@@ -1,24 +1,14 @@
 document.addEventListener("DOMContentLoaded", function () {
     const links = document.querySelectorAll(".top-menu a");
-    const sections = document.querySelectorAll("main section");
-
-    if (links.length && sections.length) {
-        links[0].classList.add("active");
-        sections[0].classList.add("active");
-    }
 
     links.forEach((link) => {
         link.addEventListener("click", function (event) {
             event.preventDefault();
             const targetId = this.getAttribute("href").substring(1);
-
-            links.forEach((l) => l.classList.remove("active"));
-            sections.forEach((s) => s.classList.remove("active"));
-
-            this.classList.add("active");
             const targetSection = document.getElementById(targetId);
             if (targetSection) {
-                targetSection.classList.add("active");
+                targetSection.scrollIntoView({ behavior: "smooth" });
+                history.pushState(null, "", `#${targetId}`);
             }
         });
     });
